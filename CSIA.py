ab3c8a9c1792a993fb7a2a5683c39bf9951f94ed
KSB2003 <65454241+KSB2003@users.noreply.github.com>
2020-09-17 22:58:05 +0530

    Add files via upload

--- CSIA.py
+++ CSIA.py
@@ -1,3 +1,11 @@
+"""
+
+Author: Kesav Bobba
+Title: Breast cancer GUI application
+Date: 16th september 2020
+
+"""
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -104,6 +112,9 @@ class logisticclassifier():
 
 
     def getweightage(self):
+        """
+        This class is determining the variables with the highest weightage in the result. By doing this we are able to select the variables for the short response for the best possible regressor.
+        """
         weightages = self.classer.coef_[0]
         weightages = list(weightages)
         for i in range(0, len(weightages)):
@@ -134,11 +145,12 @@ class logisticclassifier():
 
 
 
-
-
-
-
 def runner():
+
+    """
+    test runner, doesnt, return anything, just to check accuracy score and confusion matrix.
+    :return:
+    """
     refData = DataPre('csIA.csv')
     refData.datareader()
     xtrain, xtest, ytrain, ytest = refData.splitterandmodify()
@@ -155,10 +167,21 @@ def runner():
 
 
 class GUI():
+    """
+    It is the GUI object, it cvontains all of the app formatting and is linked to the regressor in the above function.
+    """
     def __init__(self, varlist, classer, scaler1):
+        """
+
+        This is the init function and it also intializes the variables that need to be looked at.
+        """
         self.varlist = varlist
         # self.varlist1 = varlist1
 
+        """
+        This is the variable list, it contains all of the variables for the long answer, it contains a total of 30 variables all of which have some degree of weightage and 
+        play roles in the determination of whether or not someone has breast cancer. 
+        """
         self.varlist1 = ["radius_mean", "texture_mean", "perimeter_mean", "area_mean", "smoothness_mean", "compactness_mean",
                     "concavity_mean", "concave points_mean", "symmetry_mean", "fractal_dimension_mean", "radius_se",
                     "texture_se", "perimeter_se", "area_se", "smoothness_se", "compactness_se", "concavity_se",
@@ -171,7 +194,9 @@ class GUI():
 
 
 
-
+        """
+        This is the first GUI object, it is the opening page that offers the option between the long answer response and the short answer response. 
+        """
         guiObj = Tk()
         guiObj.title('BREAST CANCER PREDICTOR')
         guiObj.configure(background='#ff00c3')
@@ -192,6 +217,11 @@ class GUI():
 
 
     def shortsurvey(self):
+
+        """
+        This method contains the page for the short function, every GUI object on Tkinter is a new page, and button require commands to be able to work.
+        That is why short survey is the command to the short survey button.
+        """
         guiObj2 = Tk()
         guiObj2.title("Short survey for breast cancer prediction")
         guiObj2.configure(background='#ff00c3')
@@ -292,6 +322,12 @@ class GUI():
 
 
     def longsurvey(self):
+
+        """
+        This is the page for the long regressor, we must use this pager in order to be able to enter in the long values
+        with the help of the long values which may not always be available we can get the most accurate possible result,
+        in fact the accuracy may range upto 97.3% which is better just to make sure.
+        """
         guiObj3 = Tk()
         guiObj3.title("Long data response for breast cancer prediction.")
         guiObj3.configure(background='#ff00c3')
@@ -300,62 +336,91 @@ class GUI():
 
         label1 = Label(guiObj3, text=self.varlist1[0])
         label1.grid(row=0, column=0)
+
         label2 = Label(guiObj3, text=self.varlist1[1])
         label2.grid(row=1, column=0)
+
         label3 = Label(guiObj3, text=self.varlist1[2])
         label3.grid(row=2, column=0)
+
         label4 = Label(guiObj3, text=self.varlist1[3])
         label4.grid(row=3, column=0)
+
         label5 = Label(guiObj3, text=self.varlist1[4])
         label5.grid(row=4, column=0)
+
         label6 = Label(guiObj3, text=self.varlist1[5])
         label6.grid(row=5, column=0)
+
         label7 = Label(guiObj3, text=self.varlist1[6])
         label7.grid(row=6, column=0)
+
         label8 = Label(guiObj3, text=self.varlist1[7])
         label8.grid(row=7, column=0)
+
         label9 = Label(guiObj3, text=self.varlist1[8])
         label9.grid(row=8, column=0)
+
         label10 = Label(guiObj3, text=self.varlist1[9])
         label10.grid(row=9, column=0)
+
         label11 = Label(guiObj3, text=self.varlist1[10])
         label11.grid(row=10, column=0)
+
         label12 = Label(guiObj3, text=self.varlist1[11])
         label12.grid(row=11, column=0)
+
         label13 = Label(guiObj3, text=self.varlist1[12])
         label13.grid(row=0, column=2)
+
         label14 = Label(guiObj3, text=self.varlist1[13])
         label14.grid(row=1, column=2)
+
         label15 = Label(guiObj3, text=self.varlist1[14])
         label15.grid(row=2, column=2)
+
         label16 = Label(guiObj3, text=self.varlist1[15])
         label16.grid(row=3, column=2)
+
         label17 = Label(guiObj3, text=self.varlist1[16])
         label17.grid(row=4, column=2)
+
         label18 = Label(guiObj3, text=self.varlist1[17])
         label18.grid(row=5, column=2)
+
         label19 = Label(guiObj3, text=self.varlist1[18])
         label19.grid(row=6, column=2)
+
         label20 = Label(guiObj3, text=self.varlist1[19])
         label20.grid(row=7, column=2)
+
         label21 = Label(guiObj3, text=self.varlist1[20])
         label21.grid(row=8, column=2)
+
         label22 = Label(guiObj3, text=self.varlist1[21])
         label22.grid(row=9, column=2)
+
         label23 = Label(guiObj3, text=self.varlist1[22])
         label23.grid(row=10, column=2)
+
         label24 = Label(guiObj3, text=self.varlist1[23])
         label24.grid(row=11, column=2)
+
         label25 = Label(guiObj3, text=self.varlist1[24])
         label25.grid(row=0, column=4)
+
         label26 = Label(guiObj3, text=self.varlist1[25])
         label26.grid(row=1, column=4)
+
         label27 = Label(guiObj3, text=self.varlist1[26])
         label27.grid(row=2, column=4)
+
         label28 = Label(guiObj3, text=self.varlist1[27])
         label28.grid(row=3, column=4)
+
         label29 = Label(guiObj3, text=self.varlist1[28])
         label29.grid(row=4, column=4)
+
         label30 = Label(guiObj3, text=self.varlist1[29])
         label30.grid(row=5, column=4)
 
@@ -392,62 +457,91 @@ class GUI():
 
         entry1 = Entry(guiObj3, textvariable=self.double1)
         entry1.grid(row=0, column=1)
+
         entry2 = Entry(guiObj3, textvariable=self.double2)
         entry2.grid(row=1, column=1)
+
         entry3 = Entry(guiObj3, textvariable=self.double3)
         entry3.grid(row=2, column=1)
+
         entry4 = Entry(guiObj3, textvariable=self.double4)
         entry4.grid(row=3, column=1)
+
         entry5 = Entry(guiObj3, textvariable=self.double5)
         entry5.grid(row=4, column=1)
+
         entry6 = Entry(guiObj3, textvariable=self.double6)
         entry6.grid(row=5, column=1)
+
         entry7 = Entry(guiObj3, textvariable=self.double7)
         entry7.grid(row=6, column=1)
+
         entry8 = Entry(guiObj3, textvariable=self.double8)
         entry8.grid(row=7, column=1)
+
         entry9 = Entry(guiObj3, textvariable=self.double9)
         entry9.grid(row=8, column=1)
+
         entry10 = Entry(guiObj3, textvariable=self.double10)
         entry10.grid(row=9, column=1)
+
         entry11 = Entry(guiObj3, textvariable=self.double11)
         entry11.grid(row=10, column=1)
+
         entry12 = Entry(guiObj3, textvariable=self.double12)
         entry12.grid(row=11, column=1)
+
         entry13 = Entry(guiObj3, textvariable=self.double13)
         entry13.grid(row=0, column=3)
+
         entry14 = Entry(guiObj3, textvariable=self.double14)
         entry14.grid(row=1, column=3)
+
         entry15 = Entry(guiObj3, textvariable=self.double15)
         entry15.grid(row=2, column=3)
+
         entry16 = Entry(guiObj3, textvariable=self.double16)
         entry16.grid(row=3, column=3)
+
         entry17 = Entry(guiObj3, textvariable=self.double17)
         entry17.grid(row=4, column=3)
+
         entry18 = Entry(guiObj3, textvariable=self.double18)
         entry18.grid(row=5, column=3)
+
         entry19 = Entry(guiObj3, textvariable=self.double19)
         entry19.grid(row=6, column=3)
+
         entry20 = Entry(guiObj3, textvariable=self.double20)
         entry20.grid(row=7, column=3)
+
         entry21 = Entry(guiObj3, textvariable=self.double21)
         entry21.grid(row=8, column=3)
+
         entry22 = Entry(guiObj3, textvariable=self.double22)
         entry22.grid(row=9, column=3)
+
         entry23 = Entry(guiObj3, textvariable=self.double23)
         entry23.grid(row=10, column=3)
+
         entry24 = Entry(guiObj3, textvariable=self.double24)
         entry24.grid(row=11, column=3)
+
         entry25 = Entry(guiObj3, textvariable=self.double25)
         entry25.grid(row=0, column=5)
+
         entry26 = Entry(guiObj3, textvariable=self.double26)
         entry26.grid(row=1, column=5)
+
         entry27 = Entry(guiObj3, textvariable=self.double27)
         entry27.grid(row=2, column=5)
+
         entry28 = Entry(guiObj3, textvariable=self.double28)
         entry28.grid(row=3, column=5)
+
         entry29 = Entry(guiObj3, textvariable=self.double29)
         entry29.grid(row=4, column=5)
+
         entry30 = Entry(guiObj3, textvariable=self.double30)
         entry30.grid(row=5, column=5)
 
@@ -487,6 +581,10 @@ class GUI():
 
 
 def runner1():
+    """
+    This part of the code is called the runner, it runs the code for the regressor and gives the coder control over the
+    program so that he can run it whenever he wants to.
+    """
     refData = DataPre('csIA.csv')
     refData.datareader()
     xtrain, xtest, ytrain, ytest = refData.splitterandmodify()
